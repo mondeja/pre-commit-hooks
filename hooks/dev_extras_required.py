4887c3986c039b22e8b6092fc32995d505a6cbac
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-03-10 21:58:25 +0100

    Try with other

--- hooks/dev_extras_required.py
+++ hooks/dev_extras_required.py
@@ -229,7 +229,10 @@ def check_setup_py(
         def _extract_extras(self, node):
             extras = {}
 
-            keys = [getattr(const, ast_str_value_attr) for const in node.value.keys]
+            keys = [
+                getattr(const, ast_str_value_attr)
+                for const in getattr(node, ast_str_value_attr).keys
+            ]
             for i in range(len(keys)):
                 values = []
                 for elt in node.value.values[i].elts:
